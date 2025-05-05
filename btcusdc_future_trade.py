d27c73a147d7abb9c861f02b25dc71e6ab408184
Rodney <rodneyruan@gmail.com>
2025-05-05 17:01:40 +0800

    Update btcusdc_future_trade.py

--- btcusdc_future_trade.py
+++ btcusdc_future_trade.py
@@ -9,7 +9,7 @@ from key_config import apikey
 from key_config import apisecret
 
 # Binance Futures configuration
-client = Client(apikey, apisecret, testnet=True)
+client = Client(apikey, apisecret)
 
 # Trading parameters
 symbol = 'BTCUSDC'
